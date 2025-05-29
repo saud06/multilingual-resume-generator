c9cecbc73302a3dd350ace05a15d968df36ce836
saud06 <saud.mn6@gmail.com>
2025-05-29 10:10:20 +0200

    Disable ESLint and TypeScript errors for production build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
