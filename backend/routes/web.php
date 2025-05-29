9116133bfb71c190541528670f8906c04f627714
saud06 <saud.mn6@gmail.com>
2025-05-29 10:10:20 +0200

    Add test route to debug routing issues

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'Web routes working', 'timestamp' => now()]);
+});
